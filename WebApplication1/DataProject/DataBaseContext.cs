a4edf9523de0dbfd9117f77196d0011fbce844f5
Oros_Christian <Oros.Da.Christian@utcluj.didatec.ro>
2022-03-29 19:59:30 +0300

    start!

---
+++ WebApplication1/DataProject/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using DataProject.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+
+namespace DataProject
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext() : base("name = testconn") { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();//opesere sa imi puna "s" la finalul tabelelor
+            //modelBuilder.Entity<Persoane>().ToTable("Persoane");
+            //modelBuilder.Entity<Persoane(aici vine numele tabelului din clasa)>().ToTable("Persoane"(sub ce denumire sa apara tabelul));
+        }
+
+    }
+}
---
+++ WebApplication1/Repository/UsersRepo.cs
@@ -0,0 +1,25 @@
+﻿using DataProject;
+using DataProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UsersRepo
+    {
+        public DataBaseContext context;
+        public UsersRepo(DataBaseContext context)
+        {
+            this.context = context;
+        }
+        public List<User> GetUser()
+        {
+            
+
+            return context.Users.ToList();
+        }
+    }
+}
