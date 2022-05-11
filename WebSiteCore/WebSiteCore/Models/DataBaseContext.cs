589a61e1ce4e834a2fb3b2b2431c4c86a57fe85d
Oros_Christian <Oros.Da.Christian@utcluj.didatec.ro>
2022-05-11 16:27:36 +0300

    no message

--- WebSiteCore/WebSiteCore/Models/DataBaseContext.cs
+++ WebSiteCore/WebSiteCore/Models/DataBaseContext.cs
@@ -12,6 +12,8 @@ namespace WebSiteCore.Models
 
         }
         //public DbSet<User> Users { get; set; }
+
+        public DbSet<Base> Base { get; set; }
         public DbSet<Auto> Auto { get; set; }
         public DbSet<Order> Order { get; set; }
        
